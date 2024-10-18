723333d263658026c4e9c3c6f40738bcc0b26539
AireneSitumorang <irensitumorang2020@gmail.com>
2024-10-18 11:32:14 +0700

    MENCOBA LAGI

--- M01.java
+++ M01.java
@@ -8,45 +8,47 @@ public class M01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String nilaihuruf;
-        double angka, peforma;
-        String indeks;
+        int totalkredit, indeks;
+        double performa, indekssementara, performaakhir, nilaiangka;
+        double a, aB, b, bC, c, d, e;
+        String nilaihuruf, kredit;
 
-        indeks = input.nextLine();
-        while (!indeks.equals("---")) {
+        totalkredit = 4.0;
+        kredit = input.nextLine();
+        while (!kredit.equals("---")) {
             nilaihuruf = input.nextLine();
             if (nilaihuruf.equals("A")) {
-                angka = 4.0;
+                nilaiangka = 4.0;
             } else {
                 if (nilaihuruf.equals("AB")) {
-                    angka = 3.5;
+                    nilaiangka = 3.5;
                 } else {
                     if (nilaihuruf.equals("B")) {
-                        angka = 3.0;
+                        nilaiangka = 3.0;
                     } else {
                         if (nilaihuruf.equals("BC")) {
-                            angka = 2.5;
+                            nilaiangka = 2.5;
                         } else {
                             if (nilaihuruf.equals("C")) {
-                                angka = 2.0;
+                                nilaiangka = 2.0;
                             } else {
                                 if (nilaihuruf.equals("D")) {
-                                    angka = 1.0;
+                                    nilaiangka = 1.0;
                                 } else {
-                                    angka = 0.0;
+                                    if (nilaihuruf.equals("E")) {
+                                        nilaiangka = 0.0;
+                                    } else {
+                                        nilaiangka = 0.0;
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-            peforma = Integer.parseInt(indeks) * angka;
-            System.out.println(toFixed(peforma,1));
-            indeks = input.nextLine();
+            performa = totalkredit * nilaiangka;
+            System.out.println(performa);
+            kredit = input.nextLine();
         }
-    }
-    
-    private static String toFixed(double value, int digits) {
-        return String.format("%." + digits + "f", value);
     }
 }
